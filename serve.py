9910f544a223b8793d518209a88f0bfa65970435
aaryamehta25 <92770840+aaryamehta25@users.noreply.github.com>
2024-01-27 17:32:33 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def stress_cpu():
+
+    subprocess.Popen(["python3", "stress_cpu.py"])
+    return jsonify({"message": "Stressing CPU started successfully!"})
+
+@app.route("/", methods=["GET"])
+def get_private_ip():
+    private_ip = socket.gethostbyname(socket.gethostname())
+    return jsonify({"private_ip": private_ip})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
